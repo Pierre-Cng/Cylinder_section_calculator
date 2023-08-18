ed1917005567373b3127072da547944cbfbddd94
Pierre Congiu <pierre.congiu@estaca.eu>
2023-08-18 15:48:23 -0400

    add notes and a test for specific shape when over length

--- test.py
+++ test.py
@@ -1,5 +1,46 @@
+import numpy as np
 import matplotlib.pyplot as plt 
 
-plt.vlines(x=0, ymin=-10, ymax=10, colors='green', ls=':', lw=2, label='vline_single - full height')
+semi_major = 10
+semi_minor = 5
+t = np.linspace(0, 2*np.pi)
+
+def ellipse_eq(semi_minor, semi_major, t):
+    x =semi_major * np.cos(t)
+    y = semi_minor * np.sin(t)
+    return x, y
+
+def tronc_ellipse():
+    x = []
+    y = []
+    t1 = np.linspace(-0.25*np.pi, 0)
+    for i in t1:
+        x.append(7)
+        y.append(-3)
+    t2 = np.linspace(0, 0.25*np.pi)
+    for i in t2:
+        x.append(7)
+        y.append(3)
+    t3 = np.linspace(0.25*np.pi, 0.75*np.pi)
+    for i in t3:
+        x.append(semi_major * np.cos(i))
+        y.append(semi_minor * np.sin(i))
+    t4 = np.linspace(0.75*np.pi, 0)
+    for i in t4:
+        x.append(-7)
+        y.append(3)
+    t5 = np.linspace(0, -0.75*np.pi)
+    for i in t5:
+        x.append(-7)
+        y.append(-3)
+    t6 = np.linspace(-0.75*np.pi, -0.25*np.pi)
+    for i in t6:
+        x.append(semi_major * np.cos(i))
+        y.append(semi_minor * np.sin(i))
+
+    return x, y
+
+x, y = tronc_ellipse()
+plt.plot(x, y)
 
 plt.show()

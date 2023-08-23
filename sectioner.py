b49604387b79406ae7d4eb85a59a746156f7795d
Pierre Congiu <pierre.congiu@estaca.eu>
2023-08-23 11:40:56 -0400

    main code ready - sectioner.py functional

--- sectioner.py
+++ sectioner.py
@@ -41,23 +41,35 @@ class Cylinder:
         return volume
     
     def vertex_parameter(self, semi_minor_axis, semi_major_axis, section_length):
+        '''
+        Function to give the vertex coordinates and parametric angle to plot the curve.
+        '''
         abs_x = section_length
         abs_y = np.sqrt(1-((abs_x/semi_major_axis)**2))*semi_minor_axis
         vertex_angle = np.arccos(abs_x/semi_major_axis)
         return abs_x, abs_y, vertex_angle
 
     def curve(self, x, y, lim, semi_minor_axis, semi_major_axis):
+        '''
+        Function returning list of x, y coordinates to draw ellipse curve in a parametric angle interval.
+        '''
         for i in np.linspace(lim[0], lim[1]):
             x.append(semi_major_axis * np.cos(i))
             y.append(semi_minor_axis * np.sin(i))
 
     def ellipse_eq(self, semi_minor_axis, semi_major_axis):
+        '''
+        Function returning x, y coordinates to draw an ellipse (specific case of curve function defined between 0 and 2pi).
+        '''
         x = []
         y = []
         self.curve(x, y, [0, 2 * np.pi], semi_minor_axis, semi_major_axis)
         return x, y
     
-    def tronc_ellipse_eq(self, semi_minor_axis, semi_major_axis, section_length):
+    def trunc_ellipse_eq(self, semi_minor_axis, semi_major_axis, section_length):
+        '''
+        Function returning x, y coordinates to draw a truncated ellipse.
+        '''
         x = []
         y = []
         abs_x, abs_y, vertex_angle = self.vertex_parameter(semi_minor_axis, semi_major_axis, section_length)
@@ -66,19 +78,13 @@ class Cylinder:
             for tuple in [(pos_neg,-pos_neg), (pos_neg,pos_neg)]:
                 x.append(tuple[0] * abs_x)
                 y.append(tuple[1] * abs_y) 
-            
             self.curve(x, y, [vertex_angle - i * np.pi, (1 - i) * np.pi - vertex_angle], semi_minor_axis, semi_major_axis)
         return x, y
-    '''
-     for i in range (2):
-            for j in range(2):
-                self.extrem_point(x, y, 1 - 2 * i, 1 - 2 * j, semi_minor_axis, cut_length, max_length)
-            self.curve(x, y, [self.t_limit(cut_length, max_length) - i * np.pi, (1 - i) * np.pi -self.t_limit(cut_length, max_length)], semi_minor_axis, semi_major_axis)
-        return x, y'''
-    
-
-    
+   
     def rectangle_eq(self, semi_height, semi_width):
+        '''
+        function returning x, y coordinates to draww a rectangle.
+        '''
         x = []
         y = []
         for tuple in [(1,-1), (1,1), (-1,1), (-1,-1), (1,-1)]:
@@ -86,27 +92,25 @@ class Cylinder:
                 y.append(tuple[1] * semi_height) 
         return x, y 
            
-
     def section_dim(self, section_angle, diameter, cylinder_length):
         '''
-        Function to calculate section dimension according to the cut angle.
+        Function returning x,y coordiates of section curve according to the cut angle.
         '''
         semi_minor_axis = diameter / 2
+        half_cylinder_length = cylinder_length / 2
         if section_angle < 90:
             semi_major_axis = semi_minor_axis / round(math.cos(math.radians(section_angle)), 5)
         opposite = semi_minor_axis * round(math.tan(math.radians(section_angle)), 5)
 
-        if opposite <= cylinder_length:
+        if opposite <= half_cylinder_length:
             return self.ellipse_eq(semi_minor_axis, semi_major_axis)
         else:
-            section_length = cylinder_length / round(math.cos(math.radians(90 - section_angle)), 5)
+            section_length = half_cylinder_length / round(math.cos(math.radians(90 - section_angle)), 5)
             if section_angle == 90:
                 return self.rectangle_eq(semi_minor_axis, section_length)
             else:
-                return self.tronc_ellipse_eq(semi_minor_axis, semi_major_axis, section_length)
-            
-            
-
+                return self.trunc_ellipse_eq(semi_minor_axis, semi_major_axis, section_length)
+                    
 class Plot:
     '''
     Class Plot to build a figure and show results 
@@ -121,15 +125,15 @@ class Plot:
         if angle == 0:
             axe.vlines(x=0, ymin=-diameter/2, ymax=diameter/2, linewidth=2, linestyle = 'dashed', color='r')
         else:
-            axe.axline((0 , 0), slope= 1/round(math.tan(math.radians(angle)), 5), linewidth=2, linestyle = 'dashed', color='r')
+            axe.axline((0 , 0), slope= 1/np.tan(np.radians(angle)) , linewidth=2, linestyle = 'dashed', color='r')
 
     def subplot_section(self, axe, angle, diameter, length):
         '''
         Function to draw an ellipse in the given subplot according to parameters.
         '''
         x, y = Cylinder().section_dim(angle, diameter, length)
-        axe.set_xlim(- diameter, diameter)
-        axe.plot(y, x)
+        axe.axis('equal')
+        axe.plot(x,y)
         axe.set_title('Section shape')
 
     def subplot_cylinder(self, axe, angle, diameter, length):
@@ -158,7 +162,7 @@ class Plot:
         self.subplot_cylinder(axes[1], angle, diameter, length)
 
         ax_slider = plt.axes([0.15, 0.1, 0.65, 0.03])
-        slider = Slider(ax_slider, 'angle', 0.0, 90.0, 1.0)
+        slider = Slider(ax_slider, 'angle', 0.0, 90.0, angle)
         def update(val):
             angle = slider.val
             axes[0].clear()
@@ -168,11 +172,4 @@ class Plot:
         slider.on_changed(update)
         plt.show()
 
-#Plot().graph(0.1, 1, 10)
-
-x, y= Cylinder().tronc_ellipse_eq(5, 25.866528711846872, 10.001900361068603)
-plt.plot(x,y)
-
-
-
-plt.show()
+Plot().graph(0, 1, 10)
